72779e500c27ea99fd468630c048c4e75e5db6b4
Stefanello <eduardo@stefanello.cc>
2022-10-23 18:23:58 -0300

    #5 - Modelagem
    Modelagem do app edificio

---
+++ edificio/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.utils.translation import gettext as _
+
+from core.models import PmModel, PmModelEdificio
+
+
+class Edificio(PmModel):
+    nome = models.CharField(max_length=120)
+    sistemas = models.ManyToManyField(to='sistema.Sistema', through='EdificioSistemas')
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = _('edifício')
+
+
+class EdificioSistemas(PmModelEdificio):
+    sistema = models.ForeignKey(to='sistema.Sistema', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'edificio_edificio_sistemas'
