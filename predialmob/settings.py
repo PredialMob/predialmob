39d2365f585ec116e188dded347548274fb9e119
Stefanello <eduardo@stefanello.cc>
2022-10-23 16:31:49 -0300

    #1 - Login
    Implementação da view de login padrão do Django com o template do Protend

--- predialmob/settings.py
+++ predialmob/settings.py
@@ -112,11 +112,17 @@ USE_I18N = True
 
 USE_TZ = True
 
+LOGIN_REDIRECT_URL = "/"
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
